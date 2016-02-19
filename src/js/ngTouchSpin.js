3185957cfae364b7e3aab4730a3e176196853375
Julien Déray <julien@deray.fr>
2016-02-19 12:10:01 +0000

    removes key actions to fix manual value entry

--- src/js/ngTouchSpin.js
+++ src/js/ngTouchSpin.js
@@ -1,241 +1,192 @@
 'use strict';
 
 angular.module('jkuri.touchspin', [])
-  .provider('ngTouchSpin', ngTouchSpinProvider)
-  .directive('ngTouchSpin', ngTouchSpinDirective);
+    .directive('ngTouchSpin', ngTouchSpinDirective);
 
-ngTouchSpinDirective.$inject = ['$timeout', '$interval', '$document', 'ngTouchSpin'];
-function ngTouchSpinDirective($timeout, $interval, $document, ngTouchSpin) {
+ngTouchSpinDirective.$inject = ['$timeout', '$interval', '$document'];
+function ngTouchSpinDirective($timeout, $interval, $document) {
 
-	var key_codes = {
-		left  : 37,
-		right : 39
-	};
+    return {
+        restrict: 'EA',
+        require: '?ngModel',
+        scope: {
+            min: '=?',
+            max: '=?',
+            step: '=?',
+            prefix: '=?',
+            postfix: '=?',
+            decimals: '=?',
+            stepInterval: '=?',
+            stepIntervalDelay: '=?',
+            initval: '=?',
+            val: '=?',
+            onChange: '=?'
+        },
+        replace: true,
+        link: function (scope, element, attrs, ngModel) {
 
-	return {
-		restrict: 'EA',
-		require: '?ngModel',
-		scope: {
-			min: '=?',
-			max: '=?',
-			step: '=?',
-			prefix: '=?',
-			postfix: '=?',
-			decimals: '=?',
-			stepInterval: '=?',
-			stepIntervalDelay: '=?',
-			initval: '=?',
-			val: '=?',
-			onChange: '=?'
-		},
-		replace: true,
-		link: function (scope, element, attrs, ngModel) {
+            initScope();
 
-			initScope();
+            function initScope() {
+                scope.min = parseFloat(Number(scope.min)) || 0;
+                scope.max = parseFloat(Number(scope.max)) || 100;
+                scope.step = scope.step || 1;
+                scope.prefix = scope.prefix || undefined;
+                scope.postfix = scope.postfix || undefined;
+                scope.decimals = parseInt(scope.decimals) || 0;
+                scope.stepInterval = scope.stepInterval || 100;
+                scope.stepIntervalDelay = scope.stepIntervalDelay || 500;
+                scope.initval = scope.initval || '';
+                scope.val = scope.value || scope.initval;
+                scope.onChange = scope.onChange || function () {};
+            }
 
-			function initScope() {
-				scope.min = parseFloat(Number(scope.min)) || 0;
-				scope.max = parseFloat(Number(scope.max)) || 100;
-				scope.step = scope.step || 1;
-				scope.prefix = scope.prefix || undefined;
-				scope.postfix = scope.postfix || undefined;
-				scope.decimals = scope.decimals || 0;
-				scope.stepInterval = scope.stepInterval || 100;
-				scope.stepIntervalDelay = scope.stepIntervalDelay || 500;
-				scope.initval = scope.initval || '';
-				scope.val = scope.value || scope.initval;
-				scope.onChange = scope.onChange || function() {};
-			}
+            var $body = $document.find('body');
+            var timeout, timer, helper = true, oldval = scope.val, clickStart;
 
-			var $body = $document.find('body');
-			var timeout, timer, helper = true, oldval = scope.val, clickStart;
+            ngModel.$setViewValue(scope.val);
+            scope.onChange(scope.val);
 
-			ngModel.$setViewValue(scope.val);
-			scope.onChange(scope.val);
+            scope.focused = false;
 
-			scope.focused = false;
+            scope.decrement = function () {
+                oldval = scope.val;
+                var value = parseFloat(parseFloat(Number(scope.val)) - parseFloat(scope.step)).toFixed(scope.decimals);
 
-			scope.decrement = function () {
-				oldval = scope.val;
-				var value = parseFloat(parseFloat(Number(scope.val)) - parseFloat(scope.step)).toFixed(scope.decimals);
+                if (value < scope.min) {
+                    value = parseFloat(scope.min).toFixed(scope.decimals);
+                    scope.val = value;
+                    ngModel.$setViewValue(value);
+                    return;
+                }
 
-				if (value < scope.min) {
-					value = parseFloat(scope.min).toFixed(scope.decimals);
-					scope.val = value;
-					ngModel.$setViewValue(value);
-					return;
-				}
+                scope.val = value;
+                ngModel.$setViewValue(value);
+                scope.onChange(scope.val);
+            };
 
-				scope.val = value;
-				ngModel.$setViewValue(value);
-				scope.onChange(scope.val);
-			};
+            scope.increment = function () {
+                oldval = scope.val;
+                var value = parseFloat(parseFloat(Number(scope.val)) + parseFloat(scope.step)).toFixed(scope.decimals);
 
-			scope.increment = function () {
-				oldval = scope.val;
-				var value = parseFloat(parseFloat(Number(scope.val)) + parseFloat(scope.step)).toFixed(scope.decimals);
+                if (value > scope.max) return;
 
-				if (value > scope.max) return;
+                scope.val = value;
+                ngModel.$setViewValue(value);
+                scope.onChange(scope.val);
+            };
 
-				scope.val = value;
-				ngModel.$setViewValue(value);
-				scope.onChange(scope.val);
-			};
+            scope.startSpinUp = function () {
+                scope.checkValue();
+                scope.increment();
 
-			scope.startSpinUp = function () {
-				scope.checkValue();
-				scope.increment();
+                clickStart = Date.now();
+                scope.stopSpin();
 
-				clickStart = Date.now();
-				scope.stopSpin();
+                $timeout(function () {
+                    timer = $interval(function () {
+                        scope.increment();
+                    }, scope.stepInterval);
+                }, scope.stepIntervalDelay);
+            };
 
-				$timeout(function() {
-					timer = $interval(function() {
-						scope.increment();
-					}, scope.stepInterval);
-				}, scope.stepIntervalDelay);
-			};
+            scope.startSpinDown = function () {
+                scope.checkValue();
+                scope.decrement();
 
-			scope.startSpinDown = function () {
-				scope.checkValue();
-				scope.decrement();
+                clickStart = Date.now();
 
-				clickStart = Date.now();
+                var timeout = $timeout(function () {
+                    timer = $interval(function () {
+                        scope.decrement();
+                    }, scope.stepInterval);
+                }, scope.stepIntervalDelay);
+            };
 
-				var timeout = $timeout(function() {
-					timer = $interval(function() {
-						scope.decrement();
-					}, scope.stepInterval);
-				}, scope.stepIntervalDelay);
-			};
+            scope.stopSpin = function () {
+                if (Date.now() - clickStart > scope.stepIntervalDelay) {
+                    $timeout.cancel(timeout);
+                    $interval.cancel(timer);
+                } else {
+                    $timeout(function () {
+                        $timeout.cancel(timeout);
+                        $interval.cancel(timer);
+                    }, scope.stepIntervalDelay);
+                }
+            };
 
-			scope.stopSpin = function () {
-				if (Date.now() - clickStart > scope.stepIntervalDelay) {
-					$timeout.cancel(timeout);
-					$interval.cancel(timer);
-				} else {
-					$timeout(function() {
-						$timeout.cancel(timeout);
-						$interval.cancel(timer);
-					}, scope.stepIntervalDelay);
-				}
-			};
+            scope.checkValue = function () {
+                var val;
 
-			scope.checkValue = function () {
-				var val;
+                if (scope.val !== '' && !scope.val.match(/^-?(?:\d+|\d*\.\d+)$/i)) {
+                    val = oldval !== '' ? parseFloat(oldval).toFixed(scope.decimals) : parseFloat(scope.min).toFixed(scope.decimals);
+                    scope.val = val;
+                    ngModel.$setViewValue(val);
+                }
 
-				if (scope.val !== '' && !scope.val.match(/^-?(?:\d+|\d*\.\d+)$/i)) {
-					val = oldval !== '' ? parseFloat(oldval).toFixed(scope.decimals) : parseFloat(scope.min).toFixed(scope.decimals);
-					scope.val = val;
-					ngModel.$setViewValue(val);
-				}
+                scope.focused = false;
+            };
 
-				scope.focused = false;
-			};
+            scope.updateValue = function () {
+                var newValue = scope.val;
 
-      scope.updateValue = function () {
-        var newValue = scope.val;
+                if (newValue !== '') {
+                    if (newValue.match(/^-?(?:\d+|\d*\.\d+)$/i)) {
+                        newValue = parseFloat(newValue);
+                        if (newValue < scope.min) newValue = scope.min;
+                        if (newValue > scope.max) newValue = scope.max;
+                        newValue = newValue.toFixed(scope.decimals);
+                    } else {
+                        newValue = oldval;
+                    }
+                    scope.val = newValue;
+                    ngModel.$setViewValue(newValue);
+                    scope.onChange(scope.val);
+                }
+            };
 
-        if (newValue !== '') {
-          if (newValue.match(/^-?(?:\d+|\d*\.\d+)$/i)) {
-            newValue = parseFloat(newValue);
-            if (newValue < scope.min) newValue = scope.min;
-            if (newValue > scope.max) newValue = scope.max;
-            newValue = newValue.toFixed(scope.decimals);
-          } else {
-            newValue = oldval;
-          }
-          scope.val = newValue;
-          ngModel.$setViewValue(newValue);
-			scope.onChange(scope.val);
-        }
-      };
+            scope.handleEmptyValue = function () {
+                var newValue = scope.val;
 
-      scope.handleEmptyValue = function () {
-        var newValue = scope.val;
+                if (newValue === '' && attrs.value) {
+                    newValue = attrs.value;
+                }
 
-        if (newValue === '' && attrs.value) {
-          newValue = attrs.value;
-        }
+                scope.val = newValue;
+                ngModel.$setViewValue(newValue);
+                scope.onChange(scope.val);
+            };
 
-        scope.val = newValue;
-        ngModel.$setViewValue(newValue);
-	  	scope.onChange(scope.val);
-      };
+            /**
+             * I have no idea what is causing it but the modelValue gets overwritten to an empty string.
+             * Change it back into the current value if it does!
+             */
+            function modelChanged() {
+                if (ngModel.$modelValue == '' && scope.val) ngModel.$setViewValue(scope.val);
+            }
 
-      /**
-       * I have no idea what is causing it but the modelValue gets overwritten to an empty string.
-       * Change it back into the current value if it does!
-       */
-      function modelChanged() {
-        if (ngModel.$modelValue == '' && scope.val) ngModel.$setViewValue(scope.val);
-      }
-      ngModel.$viewChangeListeners.push(modelChanged);
+            ngModel.$viewChangeListeners.push(modelChanged);
 
-			scope.focus = function () {
-				scope.focused = true;
-			};
+            scope.focus = function () {
+                scope.focused = true;
+            };
 
-			ngModel.$render = function () {
-				scope.val = ngModel.$viewValue;
-			};
+            ngModel.$render = function () {
+                scope.val = ngModel.$viewValue;
+            };
 
-      if (ngTouchSpin.arrowControlsEnabled()) {
-        $body.bind('keydown', function(event) {
-          if (!scope.focused) {
-            return;
-          }
 
-          event.preventDefault();
-
-          var which = event.which;
-
-          if (which === key_codes.right) {
-            scope.increment();
-          } else if (which === key_codes.left) {
-            scope.decrement();
-          }
-
-          scope.$apply();
-        });
-      }
-
-		},
-		template:
-		'<div class="input-group">' +
-		'  <span class="input-group-btn" ng-show="!verticalButtons">' +
-		'    <button type="button" class="btn btn-default" ng-mousedown="startSpinDown()" ng-mouseup="stopSpin()"><i class="fa fa-minus"></i></button>' +
-		'  </span>' +
-		'  <span class="input-group-addon" ng-show="prefix" ng-bind="prefix"></span>' +
-		'  <input type="text" ng-model="val" class="form-control" ng-change="updateValue()" ng-blur="handleEmptyValue()" ng-focus="focus()">' +
-		'  <span class="input-group-addon" ng-show="postfix" ng-bind="postfix"></span>' +
-		'  <span class="input-group-btn" ng-show="!verticalButtons">' +
-		'    <button type="button" class="btn btn-default" ng-mousedown="startSpinUp()" ng-mouseup="stopSpin()"><i class="fa fa-plus"></i></button>' +
-		'  </span>' +
-		'</div>'
-	};
-}
-
-function ngTouchSpinProvider() {
-  var arrowControlsEnabled = true;
-
-  /**
-   * @ngdoc method
-   * @name ngTouchSpinProvider#arrowControlsEnabled
-   * @description
-   * @param {boolean=} flag enable or disable arrow controls
-   * @returns {*} current value if used as getter or itself (chaining) if used as setter
-   */
-  this.arrowControlsEnabled = function(flag) {
-    if (angular.isDefined(flag)) {
-      arrowControlsEnabled = flag;
-      return this;
-    } else {
-      return arrowControlsEnabled;
-    }
-  };
-
-  this.$get = function () {
-    return this;
-  };
+        },
+        template: '<div class="input-group">' +
+        '  <span class="input-group-btn" ng-show="!verticalButtons">' +
+        '    <button type="button" class="btn btn-default" ng-mousedown="startSpinDown()" ng-mouseup="stopSpin()"><i class="fa fa-minus"></i></button>' +
+        '  </span>' +
+        '  <span class="input-group-addon" ng-show="prefix" ng-bind="prefix"></span>' +
+        '  <input type="text" ng-model="val" class="form-control" ng-change="updateValue()" ng-blur="handleEmptyValue()" ng-focus="focus()">' +
+        '  <span class="input-group-addon" ng-show="postfix" ng-bind="postfix"></span>' +
+        '  <span class="input-group-btn" ng-show="!verticalButtons">' +
+        '    <button type="button" class="btn btn-default" ng-mousedown="startSpinUp()" ng-mouseup="stopSpin()"><i class="fa fa-plus"></i></button>' +
+        '  </span>' +
+        '</div>'
+    };
 }
